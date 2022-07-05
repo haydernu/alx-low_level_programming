e8d0b1d599b84a3772ccf6b5989a5ed6e4985048
hayder <haydernuredin2212@gmail.com>
2022-07-05 13:42:24 -0700

    from 6-10

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,6 +12,7 @@ void jack_bauer(void)
 {
 
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+	
 	hours_max = 58;
 	hours_tens = '0';
 	while (hours_tens < '3')
@@ -34,7 +35,8 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar(minutes_tens);
 					_putchar(minutes_ones);
-					_putchar('\n');minutes_ones++;
+					_putchar('\n'); 
+					minutes_ones++;
 				}
 				minutes_ones = '0';
 				minutes_tens++;
