69ecb6ae7512485d08b9232d535acdc480ee6650
hayder <haydernuredin2212@gmail.com>
2022-07-05 12:51:58 -0700

    adjustment

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,12 @@
 void print_alphabet(void)
 
 {
-	char letter = 'a';
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 }
