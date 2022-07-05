8f00e627c5e5b88bc482e2465c116f44c7842c0b
hayder <haydernuredin2212@gmail.com>
2022-07-05 14:31:19 -0700

    from 11

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,10 +3,10 @@
 
 
 /**
- *  * main - computes and prints the sum of all the multiples
- *   * of 3 or 5 below 1024
- *    * Return: Always 0 (Success)
- *     */
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ **/
 
 int main(void)
 
