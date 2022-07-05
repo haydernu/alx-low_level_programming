6331ae22da310082b648d4ee95fe1b28a2b1885e
hayder <haydernuredin2212@gmail.com>
2022-07-05 12:58:50 -0700

    adjustment

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,9 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *
- *  * print_alphabet_x10 - print alphabet 10 times
- *
- *   * Return: void
- *
- *    */
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: void
+ **/
 
 void print_alphabet_x10(void)
 
