8c1fea94638af6be7141ccac0d819a6727497f59
hayder <haydernuredin2212@gmail.com>
2022-07-12 12:21:38 -0700

    0x05

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  *reset_to_98 - function
- *@:pointer of a variable
- */
+ *@n: pointer of a variable
+ **/
 void reset_to_98(int *n)
+
 {
-*n= 98;
+*n = 98;
 }
