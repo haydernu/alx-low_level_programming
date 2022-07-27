a4fdba1fdb21c0754ada5019c2a9e2b1cae6f951
hayder <haydernuredin2212@gmail.com>
2022-07-26 22:03:37 -0700

    0x0A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,4 @@
-#include <stdio.h>                                                                                                                                                                            
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -6,10 +6,11 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  * Return: If the program receives two arguments - 0.
- *If the program does not receive two arguments - 1.
+ * If the program does not receive two arguments - 1.
  **/
 
 int main(int argc, char *argv[])
+
 {
 int num1, num2, prod;
 
@@ -21,7 +22,6 @@ return (1);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 prod = num1 * num2;
-
 printf("%d\n", prod);
 return (0);
 }
