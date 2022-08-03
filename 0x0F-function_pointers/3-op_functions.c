705bfe4c74029a5a1a2c08f4e3e9bee64491e0a2
hayder <haydernuredin2212@gmail.com>
2022-08-03 14:36:56 -0700

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,64 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
- * Return: ...
- **/
-
+ *op_add - add a & b.
+ *@a: integer value.
+ *@b: integer value.
+ *
+ *Return: sum of a & b.
+ */
 int op_add(int a, int b)
-
 {
 return (a + b);
 }
-
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
- * Return: ...
- **/
-
+ *op_sub - subtraction a & b.
+ *@a: integer value.
+ *@b: integer value.
+ *
+ *Return: subtraction result of a & b.
+ */
 int op_sub(int a, int b)
-
 {
 return (a - b);
 }
-
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
- *  Return: ...
- **/
-
+ *op_mul - multiplication a & b.
+ *@a: integer value.
+ *@b: integer value.
+ *
+ *Return: multiplication result of a & b.
+ */
 int op_mul(int a, int b)
-
 {
 return (a * b);
 }
-
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
- * Return: ...
- **/
-
+ *op_div - division a & b.
+ *@a: integer value.
+ *@b: integer value.
+ *
+ *Return: divided result of a & b.
+ */
 int op_div(int a, int b)
-
 {
-if (b == 0)
+if (!b)
 {
 printf("Error\n");
 exit(100);
 }
 return (a / b);
 }
-
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
- * Return: ...
- **/
-
+ *op_mod -modulo of a & b.
+ *@a: integer value.
+ *@b: integer value.
+ *
+ *Return: mod result of a & b.
+ */
 int op_mod(int a, int b)
-
 {
-if (b == 0)
+if (!b)
 {
 printf("Error\n");
 exit(100);
