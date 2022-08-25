296b201defee370d6a6123815e100dc8edca679a
hayder <haydernuredin2212@gmail.com>
2022-08-25 12:32:59 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,8 @@ int create_file(const char *filename, char *text_content)
 
 int file, i, wr;
 
-if (filename == NULL)	
-return (-1);
+if (filename == NULL)
+	return (-1);
 file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (file == -1)
 return (-1);
