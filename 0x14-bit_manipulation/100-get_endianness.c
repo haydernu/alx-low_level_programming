c367934a913800b4286fb06e396a2c90c8379007
Hayder <haydernuredin2212@gmail.com>
2022-10-12 01:05:30 -0700

    0x14

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,8 +7,8 @@
 
 int get_endianness(void)
 {
-unsigned int index = 1;
-char *c = (char *) &index;
+        unsigned int index = 1;
+        char *c = (char *) &index;
 
-return (*c);
+        return (*c);
 }
