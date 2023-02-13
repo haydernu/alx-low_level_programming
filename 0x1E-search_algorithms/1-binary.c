86742bda06e939fb524ce91a57cbc5b4c9d1f763
haydernu <haydernuredin2212@gmail.com>
2023-02-13 01:11:18 -0800

    last

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,8 +3,6 @@
 /**
  * recursive_search - searches for a value in an array of
  * integers using the Binary search algorithm
- *
- *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
@@ -42,7 +40,6 @@ int recursive_search(int *array, size_t size, int value)
 /**
  * binary_search - calls to binary_search to return
  * the index of the number
- *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
